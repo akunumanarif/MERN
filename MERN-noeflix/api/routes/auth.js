7ed4153059d9a52bf7c0fc8ccf377b7b143e0969
M Nu'man Arif H <numanarif87@@gmail.com>
2022-03-14 18:39:59 +0700

    Creating JWT functionality

--- MERN-noeflix/api/routes/auth.js
+++ MERN-noeflix/api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //REGISTER FUNCTION
 router.post('/register', async (req, res) => {
@@ -29,10 +30,17 @@ router.post('/login', async (req, res) => {
 		const originalPass = decryptWithAES.toString(CryptoJS.enc.Utf8);
 
 		originalPass !== req.body.password && res.status(401).json('Wrong password');
-
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin
+			},
+			process.env.SECRET_KEY,
+			{ expiresIn: '5d' }
+		);
 		const { password, ...info } = user._doc;
 
-		res.status(200).json(info);
+		res.status(200).json({ ...info, accessToken });
 	} catch (error) {
 		res.status(500).json(error);
 	}
