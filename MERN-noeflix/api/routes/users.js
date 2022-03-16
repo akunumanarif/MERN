c2201fa0c358de6f305630229353eff91d18ff14
M Nu'man Arif H <numanarif87@@gmail.com>
2022-03-17 01:31:54 +0700

    Creating User Update Functionality with JWT/AES Encryption

---
+++ MERN-noeflix/api/routes/users.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const verify = require('../verifyToken');
+//UPDATE USER METHOD
+
+router.put('/:id', verify, async (req, res) => {
+	if (req.user.id === req.params.id || req.user.isAdmin) {
+		const isNewPassword = req.body.password;
+		const encryptWithAES = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
+		if (isNewPassword) {
+			isNewPassword = encryptWithAES;
+		}
+		try {
+			const updateUser = await User.findByIdAndUpdate(
+				req.params.id,
+				{
+					$set: req.body
+				},
+				{ new: true }
+			);
+			res.status(200).json(updateUser);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json('Forbidden access');
+	}
+});
+
+//DELETE USER METHOD
+//GET ONE USER METHOD
+//GET ALL USER METHOD
+//GET USER STATISTIC METHOD
+
+module.exports = router;
